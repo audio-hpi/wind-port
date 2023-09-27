d6b8741b8b5f1bbf6a49b246957b7536a9f74c9e
Audio Lab <audio-hpi@outlook.com>
2023-09-27 17:16:58 +0200

    small changes

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import sounddevice as sd
 import scipy.signal as ss
 from collections import deque
 
-
 FS = 44100
 BLOCK_SIZE = 1024
 CHANNELS = 1
 LOWCUT = 100 # Hz
 PLOT_LENGTH = 10
 
-rms_data = deque([0 for i in range(PLOT_LENGTH)], maxlen=PLOT_LENGTH)
+rms_data = deque([0 for _ in range(PLOT_LENGTH)], maxlen=PLOT_LENGTH)
 x = np.arange(len(rms_data))
 
 b, a = ss.butter(5, LOWCUT, fs=FS, btype='low')
@@ -20,5 +19,5 @@ def callback(indata, frames, time, status):
     rms_data.append(np.sqrt(np.sum(filtered * filtered) / len(filtered)))
 
 with sd.InputStream(callback=callback, channels=CHANNELS, samplerate=FS):
-    print('lIskauhdasd')
+    print('Listening ...')
     sd.sleep(10000)
